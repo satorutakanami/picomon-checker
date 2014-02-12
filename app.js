b5224f8f32cc2e7aae620c7720ffe95a9c30046e
storz <distorz@gmail.com>
2014-02-12 17:15:41 +0900

    add version switch

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var express = require("express");
 var logfmt = require("logfmt");
+
 var app = express();
 
 app.use(logfmt.requestLogger());
@@ -8,13 +9,15 @@ app.configure(function () {
 })
 app.set('view engine', 'ejs');
 
-app.get('/*', function(req, res) {
+var index = function(req, res) {
+	var version = req.query.ver || '1.0';
 	var env     = req.query.env || 'production';
 	var port    = req.query.port;
 	var client  = req.query.c || '000001';
 	var profile = req.query.p || '1';
 	var route   = require('url').parse(req.url).pathname;
 	var here    = req.get('host');
+	var isPost  = (req.method === 'POST') ? true : false;
 	var isHttps = req.connection.encrypted ? true : false;
 	var baseUrl, url, secure_url;
 	switch (env) {
@@ -37,8 +40,10 @@ app.get('/*', function(req, res) {
 	}
 	secure_url = 'https://' + baseUrl + ((env == 'development') ? ':' + (+port + 1) : '');
 	url        = 'http://' + baseUrl + ((env == 'development') ? ':' + (+port) : '');
+	console.log(version)
 	var params = {
 		query:      {
+			ver:     version,
 			env:     env,
 			port:    port,
 			client:  client,
@@ -51,60 +56,13 @@ app.get('/*', function(req, res) {
 		route:      route,
 		protocol:   isHttps ? 'https://' : 'http://',
 		here:       here,
-		is_post:    false
+		is_post:    isPost
 	};
 	res.render(__dirname + '/index', params);
-});
+}
 
-app.post('/*', function(req, res) {
-	console.log(req)
-	var env     = req.body.env || 'production';
-	var port    = req.body.port;
-	var client  = req.body.c || '000001';
-	var profile = req.body.p || '1';
-	var route   = require('url').parse(req.url).pathname;
-	var here    = req.get('host');
-	var isHttps = req.connection.encrypted ? true : false;
-	var baseUrl, url, secure_url;
-	switch (env) {
-		case 'development':
-			baseUrl = 'battamon.net';
-			port = port || '3010';
-			break;
-		case 'staging':
-			baseUrl = 'pacchimon.net';
-			port = '';
-			break;
-		case 'production':
-			baseUrl = 'picomon.jp';
-			port = '';
-			break;
-		default:
-			baseUrl = 'battamon.net';
-			port = port || '3010';
-			break;
-	}
-	secure_url = 'https://' + baseUrl + ((env == 'development') ? ':' + (+port + 1) : '');
-	url        = 'http://' + baseUrl + ((env == 'development') ? ':' + (+port) : '');
-	var params = {
-		query:      {
-			env:     env,
-			port:    port,
-			client:  client,
-			profile: profile
-		},
-		url:        url,
-		secure_url: secure_url,
-		client:     ('000000'+client).slice(-6),
-		profile:    profile,
-		route:      route,
-		protocol:   isHttps ? 'https://' : 'http://',
-		here:       here,
-		is_post:    true
-	};
-	res.render(__dirname + '/index', params);
-});
-
+app.get('/*', index);
+app.post('/*', index);
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
